51ff73ed07efb6cfcc674a12765aa05d03906bbf
Jagadeesh Sutar <jagadeeshsutar56883@gamil.com>
2024-10-04 22:35:56 +0530

    Adding a comment

--- mongodb tutorial/02 create.js
+++ mongodb tutorial/02 create.js
@@ -16,3 +16,6 @@ db.inventory.insertOne(
     { item: "planner", qty: 75, size: { h: 22.85, w: 30, uom: "cm" }, status: "D" },
     { item: "postcard", qty: 45, size: { h: 10, w: 15.25, uom: "cm" }, status: "A" }
  ]);
+
+
+//  Hiii How are you?
