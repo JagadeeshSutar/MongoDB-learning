715a3ab0c9b6b204a386c1fff99b09aaba30e357
Jagadeesh Sutar <jagadeeshsutar56883@gamil.com>
2024-10-04 22:39:54 +0530

    This is a new branch

--- mongodb tutorial/02 create.js
+++ mongodb tutorial/02 create.js
@@ -16,3 +16,5 @@ db.inventory.insertOne(
     { item: "planner", qty: 75, size: { h: 22.85, w: 30, uom: "cm" }, status: "D" },
     { item: "postcard", qty: 45, size: { h: 10, w: 15.25, uom: "cm" }, status: "A" }
  ]);
+
+//  HII This is a comment to check the git branches switching
